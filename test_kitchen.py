e65d50f2e0e5d9f0839504aeacb524af272dd424
pawarisa23 <pawarisa.mas@student.mahidol.ac.th>
2026-09-10 00:01:39 +0700

    A4: REFACTOR - compare whole objects in earlier tests

--- test_kitchen.py
+++ test_kitchen.py
@@ -3,7 +3,7 @@
 #
 # [x] 200 g x 3 = 600 g
 # [x] การคูณต้องไม่เปลี่ยนค่าของอ็อบเจ็กต์เดิม
-# [ ] ปริมาณสองค่าที่มีทั้งตัวเลขและหน่วยเท่ากันถือว่าเท่ากัน
+# [x] ปริมาณสองค่าที่มีทั้งตัวเลขและหน่วยเท่ากันถือว่าเท่ากัน
 # [ ] 1 oz ไม่เท่ากับ 1 g
 # [ ] 200 g + 300 g = 500 g
 # [ ] 200 g + 1 oz แปลงผลลัพธ์เป็นกรัมโดยใช้อัตราแปลงหน่วย
@@ -14,18 +14,18 @@ from kitchen import Quantity
 
 def test_multiplication():
     flour = Quantity(200)
-    assert flour.times(3).amount == 600
+    assert flour.times(3) == Quantity(600)
 
 
 def test_multiplication_by_two():
     flour = Quantity(200)
-    assert flour.times(2).amount == 400
+    assert flour.times(2) == Quantity(400)
 
 
 def test_multiplication_returns_a_new_quantity():
     flour = Quantity(200)
-    assert flour.times(3).amount == 600
-    assert flour.times(2).amount == 400
+    assert flour.times(3) == Quantity(600)
+    assert flour.times(2) == Quantity(400)
 
 
 def test_equality():
