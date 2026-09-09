65ed92c21805547cb2fee1ba8e219cb65cc19678
pawarisa23 <pawarisa.mas@student.mahidol.ac.th>
2026-09-10 00:02:43 +0700

    A6: RED - adding two quantities in the same unit

--- test_kitchen.py
+++ test_kitchen.py
@@ -9,7 +9,7 @@
 # [ ] 200 g + 1 oz แปลงผลลัพธ์เป็นกรัมโดยใช้อัตราแปลงหน่วย
 # [ ] (200 g + 1 oz) x 2
 
-from kitchen import Quantity
+from kitchen import Converter, Quantity
 
 
 def grams(amount):
@@ -43,3 +43,9 @@ def test_equality():
 
 def test_grams_are_not_ounces():
     assert grams(1) != ounces(1)
+
+
+def test_simple_addition():
+    total = grams(200).plus(grams(300))
+    converter = Converter()
+    assert converter.reduce(total, "g") == grams(500)
